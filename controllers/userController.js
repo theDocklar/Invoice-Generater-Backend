12e8334be1ace78ec68788071803d43ed71c719c
Tharindu Mishen <tharindumishen@gmail.com>
2026-05-03 11:19:46 +0530

    Update cookie sameSite attribute for production environment

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const setCookie = (res, token) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
