8376cacdc8347ff1222f7da52721615a55a0f687
shaveenf <shaveenfernando1@gmail.com>
2026-05-11 21:42:40 +0530

    Remove authentication middleware from invoice routes for testing purposes

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -17,7 +17,7 @@ import { protect } from "../middlewares/auth.js";
 const router = express.Router();
 
 // All routes require authentication
-router.use(protect);
+// router.use(protect);
 
 router.get("/next-invoice-number", getNextInvoiceNumber);
 router.post("/create-invoice", createInvoice);
